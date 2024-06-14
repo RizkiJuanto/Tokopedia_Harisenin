31fc8027f2b2147c5c1ae9ce827be960391b8240
kuanvell@gmail.com <kuanvell@gmail.com>
2024-06-14 15:22:29 +0700

    making login page

--- Tokopedia_Harisenin/src/App.jsx
+++ Tokopedia_Harisenin/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
-import Login from './pages/login'
+import { BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
-function App() {
+
+export const App = () => {
 
 
   return (
-   <>
-   <Login />
-   </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
--- Tokopedia_Harisenin/src/pages/login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const login = () => {
-  return (
-    <div>loginasdasdasd</div>
-  )
-}
-
-export default login
