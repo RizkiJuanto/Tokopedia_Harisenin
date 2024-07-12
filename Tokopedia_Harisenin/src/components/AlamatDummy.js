3dc606bf4cb6840960aa3798505da9675bee161e
Hollow05 <kuanvell@gmail.com>
2024-07-12 23:43:07 +0700

    hapus env

---
+++ Tokopedia_Harisenin/src/components/AlamatDummy.js
@@ -0,0 +1,32 @@
+let divs = [
+  {
+    id: 1,
+    title: "Rumah jkt",
+    name: "Rizki Juanto",
+    phone: "9751123121",
+    address: "Jalan test aja, blok S no 96, Kota blabla, Tangerang, kb 119",
+  },
+  {
+    id: 2,
+    title: "Rumah jkt",
+    name: "Dizki Juanto",
+    phone: "9751123121",
+    address: "Jalan test aja, blok S no 96, Kota blabla, Tangerang, kb 119",
+  },
+  {
+    id: 3,
+    title: "Rumah jkt",
+    name: "Jizki Juanto",
+    phone: "9751123121",
+    address: "Jalan test aja, blok S no 96, Kota blabla, Tangerang, kb 119",
+  },
+  {
+    id: 4,
+    title: "Rumah jkt",
+    name: "Abun",
+    phone: "9751123121",
+    address: "Jalan test aja, blok S no 96, Kota blabla, Tangerang, kb 119",
+  },
+];
+
+export default divs;
